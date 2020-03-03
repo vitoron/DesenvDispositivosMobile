ed996f15ba0165f97674ce933f479afbd3eb422e
Vitor Oliveira Nunes - 818144490 <818144490@animaedu.intranet>
2020-03-02 22:34:06 -0300

    Segunda versao do commit

--- EntregaGit.java
+++ EntregaGit.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
 public class EntregaGit {
 public static void main (String [] args){
 System.out.println ("Entregando tarefas com o Git na linha de comando...");
+String nome = JOptionPane.showInputDialog("Qual seu nome?");
+JOptionPane.showMessageDialog(null, "Oi, " + nome);
 }
 }
